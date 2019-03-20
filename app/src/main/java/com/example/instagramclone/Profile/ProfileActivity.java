57370d6cb697476c446dc063f523b0267eed78c2
gilang <gilangyulianta07@gmail.com>
2019-03-20 17:30:33 +0700

    Profile Bar

--- app/src/main/java/com/example/instagramclone/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/instagramclone/Profile/ProfileActivity.java
@@ -1,27 +1,53 @@
 package com.example.instagramclone.Profile;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.instagramclone.Like.LikesActivity;
 import com.example.instagramclone.R;
 import com.example.instagramclone.Utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class ProfileActivity extends AppCompatActivity {
-
-    //private static void String TAG = "Search Activity";
-
+public class ProfileActivity extends AppCompatActivity{
     private static final int ACTIVITY_NUM=4;
+
+    private Context mContext = ProfileActivity.this;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
 
         setupBottomNavigationVeiw();
+        setupToolbar();
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+              //  Log.d(TAG, "onMenuItemClick: clicked menu item: " + item);
+
+                switch (item.getItemId()){
+                    case R.id.profileMenu:
+                     //   Log.d(TAG, "onMenuItemClick: Navigating to Profile Preferences.");
+                }
+
+                return false;
+            }
+        });
     }
 
     private void setupBottomNavigationVeiw(){
@@ -32,4 +58,10 @@ public class ProfileActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
 }
